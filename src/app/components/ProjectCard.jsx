8fc074814cbef819f58fcf42f366842aa2f2b682
ОlexaEvtush <olexaevtush@gmail.com>
2023-12-09 04:29:46 +0200

    commit

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -6,9 +6,8 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
     <div>
       <div
-        className="h-52 rounded-t-xl relative group"
-        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
-      >
+        className="relative h-52 rounded-t-xl group"
+        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}>
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
             href={gitUrl}
@@ -17,14 +16,13 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
           </Link>
           <Link
             href={previewUrl}
-            className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
-          >
+            className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link">
             <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
           </Link>
         </div>
       </div>
       <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6">
-        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <h5 className="mb-2 text-xl font-semibold">{title}</h5>
         <p className="text-[#ADB7BE]">{description}</p>
       </div>
     </div>
