04fbd3684f31bbd76b5243b9e6a5bbe54bda731e
ОlexaEvtush <olexaevtush@gmail.com>
2023-12-10 00:18:14 +0200

    add blur-hero

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -9,12 +9,15 @@ const HeroSection = () => {
   return (
     <section className="pb-[130px]">
       <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className=" bg-[#512f6b] rounded-[15px] h-80 w-80 z-0 blur-[125px] scale-120 opacity-130 absolute top-50 -left-45 transform -translate-x-1/2 -translate-1/2 animate-blob animation-delay-2000"></div>
+        <div className=" bg-[#6c3075] rounded-[15px] h-80 w-80 z-0 blur-[125px] scale-120 opacity-130 absolute top-30 -left-55 transform -translate-x-1/2 -translate-1/2 animate-blob animation-delay-2000"></div>
+        <div className=" bg-[#46297a] rounded-[15px] h-80 w-80 z-0 blur-[125px] scale-120 opacity-130 absolute top-30 -left-65 transform -translate-x-1/2 -translate-1/2 animate-blob animation-delay-4000"></div>
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 1.5 }}
           className="col-span-8 text-center place-self-center sm:text-left justify-self-start">
-          <h1 className="mb-4 text-3xl font-bold text-[#e0cbe1] md:text-6xl sm:text-5xl lg:text-7xl lg:leading-normal">
+          <h1 className="mb-4 text-3xl font-bold text-[#f0e2f1] md:text-6xl sm:text-5xl lg:text-7xl lg:leading-normal">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-200">
               Hello, I&apos;m{" "}
             </span>
@@ -37,9 +40,9 @@ const HeroSection = () => {
             <Link
               href="/#certificate"
               className="inline-block w-full px-1 py-1 mr-4 text-white rounded-full sm:w-fit bg-[#121212] hover:bg-slate-800 bg-gradient-to-br from-primary-500 to-secondary-500">
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              <button className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Certificate
-              </span>
+              </button>
             </Link>
             <Link
               href="/"
@@ -48,7 +51,7 @@ const HeroSection = () => {
                 CV
               </span>
             </Link>
-            <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] bg-[#512f6b] rounded-[25px] h-80 w-80 z-0 blur-[85px] scale-120 opacity-95 absolute top-50 -left-45 transform -translate-x-1/2 -translate-1/2"></div>
+            
           </div>
         </motion.div>
         <motion.div
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,25 @@ module.exports = {
   cssModules: true,
   theme: {
     extend: {
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
